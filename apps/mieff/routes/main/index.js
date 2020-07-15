51c20fe93b80bf2ba864759e9d446c464d11bdeb
ponyspy <desade4me@gmail.com>
2020-07-15 19:45:43 +0300

    eve

--- apps/mieff/routes/main/index.js
+++ apps/mieff/routes/main/index.js
@@ -7,6 +7,12 @@ module.exports = function(Model) {
 	var Program = Model.Program;
 	var Place = Model.Place;
 
+	var get_locale = function(option, lg) {
+		return ((option.filter(function(locale) {
+			return locale.lg == lg;
+		})[0] || {}).value || '');
+	};
+
 	module.index = function(req, res) {
 		Program.find().exec(function(err, programs) {
 			Place.find().exec(function(err, places) {
@@ -19,7 +25,15 @@ module.exports = function(Model) {
 						{ $sort: { 'schedule.date': 1 } },
 					])
 					.exec(function(err, events) {
-						res.render('main/index.pug', {moment: moment, events: events, programs: programs, places: places, dates: dates});
+						Place.populate(events, {path: 'schedule.place'}, function(err, events) {
+							Program.populate(events, {path: 'program'}, function(err, events) {
+								res.render('main/index.pug', {
+									moment: moment,
+									get_locale: get_locale,
+									events: events, programs: programs, places: places, dates: dates
+								});
+							});
+						});
 					});
 				});
 			});
