0d4db20b93a8a7d3d7d392ba51a523099a350ff8
ponyspy <desade4me@gmail.com>
2020-07-15 19:02:53 +0300

    styl

--- apps/mieff/routes/main/index.js
+++ apps/mieff/routes/main/index.js
@@ -1,8 +1,20 @@
+var moment = require('moment');
+
 module.exports = function(Model) {
 	var module = {};
 
+	var Event = Model.Event;
+	var Program = Model.Program;
+	var Place = Model.Place;
+
 	module.index = function(req, res) {
-		res.render('main/index.pug');
+		Program.find().exec(function(err, programs) {
+			Place.find().exec(function(err, places) {
+				Event.find().distinct('schedule.date', function(err, dates) {
+					res.render('main/index.pug', {moment: moment, programs: programs, places: places, dates: dates});
+				});
+			});
+		});
 	};
 
 	return module;
