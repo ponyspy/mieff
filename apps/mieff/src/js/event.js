dc467c43c1bfb862e877e27f4b106ade52fd6acd
ponyspy <desade4me@gmail.com>
2020-07-16 19:08:11 +0300

    event

---
+++ apps/mieff/src/js/event.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	var swiper = new Swiper('.swiper-container', {
+		loop: true,
+		effect: 'fade',
+		keyboard: {
+			enabled: true
+		},
+		pagination: {
+			clickable: true,
+			el: '.swiper-pagination',
+		}
+	});
+
+	$('.cover_item').on('click', function(e) {
+		swiper.slideNext();
+	});
+});
