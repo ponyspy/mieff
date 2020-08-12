4cb91b624bb4e466559ce86139006401c7661813
ponyspy <desade4me@gmail.com>
2020-08-12 13:16:38 +0300

    news

--- apps/mieff/src/js/news.js
+++ apps/mieff/src/js/news.js
@@ -1,6 +1,6 @@
 var $window = $(window);
 var $document = $(document);
-var context = { skip: 0, limit: 10 };
+var context = { skip: 0, limit: 30 };
 
 $(function() {
 	var scrollLoader = function(e) {
@@ -10,12 +10,25 @@ $(function() {
 			$.ajax({url: '', method: 'POST', data: { context: context }, async: false }).done(function(data) {
 				if (data !== 'end') {
 
-					$('.news_posts').append(data);
+					var $posts = $(data);
+					var years = [];
 
-					context.skip += 10;
+					$posts.each(function() {
+						var $post = $(this);
+						var last_year = $('.post_item').last().attr('data-year');
+
+						if ($post.attr('data-year') !== last_year && years.indexOf(last_year) == -1) {
+							years.push(last_year);
+
+							$date_title = $('<div/>', {'class': 'date_title', 'text': $post.attr('data-year')});
+							$date_title.appendTo('.news_posts');
+						}
+
+						$post.appendTo('.news_posts');
+					});
+
+					context.skip += 30;
 					$window.on('scroll', scrollLoader);
-				} else {
-					$('.news_more').addClass('hide');
 				}
 			});
 		}
