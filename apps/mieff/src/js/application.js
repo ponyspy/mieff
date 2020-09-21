627f7233f263b54bd0b501717e26678affcb1cf7
ponyspy <desade4me@gmail.com>
2020-09-21 13:36:56 +0300

    submit

--- apps/mieff/src/js/application.js
+++ apps/mieff/src/js/application.js
@@ -1,7 +1,11 @@
 $(function() {
-	$('.submit_button').on('click', function() {
-		if ($('.confirm_check').is(':checked')) {
+	$('.submit_button').on('click', function(e) {
+		if ($(this).hasClass('active')) {
 			$('form.main').submit();
 		}
+	});
+
+	$('.confirm_check').on('change', function(e) {
+		$('.submit_button').toggleClass('active');
 	});
 });
